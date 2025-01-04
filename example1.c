a266cd669e0b187c6f6af2c22144566f15d845b7
3xg3lin <krmutku76@gmail.com>
2025-01-04 18:05:35 +0300

    Create example1.c

---
+++ example1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/* Tis is a comment */
+
+int main () {
+	printf("Hello World!\n");
+	return 0;
+}
+/* Use the -save-temps flag with gcc to see all “source code -> preprocessor -> compiler -> assembler -> linker -> binary executable” files. */

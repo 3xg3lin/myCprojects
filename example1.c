8e4d17cb43b0117e4f479ff993c07d829b0b2e40
kerem <krmutku76@gmail.com>
2025-12-02 20:17:19 +0300

    New

--- example1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-/* Tis is a comment */
-
-int main () {
-	printf("Hello World!\n");
-	return 0;
-}
-/* Use the -save-temps flag with gcc to see all “source code -> preprocessor -> compiler -> assembler -> linker -> binary executable” files. */

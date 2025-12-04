d93e0526d1599fd7c69762deb952d4ab9f86341d
kerem <krmutku76@gmail.com>
2025-12-04 15:53:48 +0300

    Created hello2.c

---
+++ Chapter1_HelloWorld/hello2.c
@@ -0,0 +1,16 @@
+/*
+ * hello2.c
+ * My first C program with comments.
+ * by 3xg3lin.
+ * created 2025/12/4
+ */
+
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello World!\n");
+  return 0;
+}
+
+/* eof */

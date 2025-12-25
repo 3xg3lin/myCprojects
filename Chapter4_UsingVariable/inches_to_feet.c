f440c5a6289a1bdf15a8bcb1959e65b707b09af4
kerem <krmutku76@gmail.com>
2025-12-25 20:18:04 +0300

    Created inches_to_feet.c

---
+++ Chapter4_UsingVariable/inches_to_feet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+double inchesToFeet( double );
+
+int main(void)
+{
+    double inches = 1024.0;
+    double feet = 0.0;
+    feet = inchesToFeet(inches);
+    printf( "%12.3g inches is equal to %12.3g feet\n", inches, feet);
+    return 0;
+}
+
+double inchesToFeet( double someInches)
+{
+    double someFeet = someInches / 12.0;
+    return someFeet ;
+}

2ee69a4294a7fcb16a9922ba67146c1607bc14ba
3xg3lin <krmutku76@gmail.com>
2025-12-31 15:49:18 +0300

    Created Inf_Nan.c

---
+++ Chapter5_ExploringOperation/Inf_Nan.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    double y = 1 / 0.0;
+    printf( "1 / 0.0 = %f\n", y);
+
+    y = -1 / 0.0;
+    printf( "-1 / 0.0 = %f\n", y);
+
+    y = log( 0 );
+    printf( "log(0) = %f\n", y);
+
+    y = sqrt( -1 );
+    printf( "Square root of -1 = %f\n", y);
+    return 0;
+}

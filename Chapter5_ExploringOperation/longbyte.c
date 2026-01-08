7ca0799c5148d55fa1a7e541904de7171df39e19
3xg3lin <krmutku76@gmail.com>
2026-01-08 11:55:56 +0300

    Created longbyte.c

---
+++ Chapter5_ExploringOperation/longbyte.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+long int add( long int i1, long int i2 ){
+    return i1 + i2;
+}
+
+int main( void ){
+    unsigned char b1 = 254;
+    unsigned char b2 = 253;
+    unsigned char r1;
+    r1 = add( b1, b2 );
+    printf( "%d + %d = %d\n", b1, b2, r1 );
+    return 0;
+}

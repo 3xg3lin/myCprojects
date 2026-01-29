a284a04c90c3355719e3166a83381347e7312247
3xg3lin <krmutku76@gmail.com>
2026-01-29 14:53:56 +0300

    Create gauss_loops.c

---
+++ Chapter7_LoopsAndIterations/gauss_loops.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int sumNviaFor( int n );
+int sumNviaWhile( int n );
+int sumNviaDoWhile( int n );
+int sumNviaGoto( int n );
+
+int main( int argc, char *argv[] ){
+    int n = 100;
+
+    printf( " The sum of 1..%d = %d (via while() ... loop)\n", n, sumNviaWhile( n ));
+}
+
+int sumNviaFor( int n ){
+
+}
+int sumNviaWhile( int n ){
+    int sum = 0;
+    int num = 0;
+
+    while ( num < n ) {
+        sum += (num + 1);
+        num++;
+    }
+    return sum;
+}
+int sumNviaDoWhile( int n ){
+
+}
+int sumNviaGoto( int n ){
+
+}

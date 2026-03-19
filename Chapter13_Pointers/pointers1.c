4c058496213742208af99b41311f3875b815e4c1
3xg3lin <krmutku76@gmail.com>
2026-03-19 14:03:23 +0300

    Created pointer1.c

---
+++ Chapter13_Pointers/pointers1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    int height = 10;
+    int width = 20;
+    int length = 40;
+
+    int* pDimension = NULL;
+
+    printf("\nValues:\n\n");
+    printf("sizeof(int) = %2lu\n", sizeof(int));
+    printf("sizeof(int*) = %2lu\n", sizeof(int*));
+    printf("[height, width, length] = [%2d, %2d, %2d] \n\n",height ,width, length);
+    printf("address of pDimension = %p\n", &pDimension);
+    printf("address kept by pDimension = %p\n", pDimension);
+    printf("value at NULL pDimension = %2d\n", *pDimension);
+
+    printf("\nUsing address of each named variables...\n\n");
+    pDimension = &height;
+    printf("address kept by pDimension(address of height) = %p, value at address = %2d\n", pDimension, *pDimension);
+    pDimension = &width;
+    printf("address kept by pDimension(address of width) = %p, value at address = %2d\n", pDimension, *pDimension);
+    pDimension = &length;
+    printf("address kept by pDimension(address of length) = %p, value at address = %2d\n", pDimension, *pDimension);
+    return 0;
+}

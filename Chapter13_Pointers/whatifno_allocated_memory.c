1f7141a85ee88b83291f939ad8b16817ff170696
3xg3lin <krmutku76@gmail.com>
2026-03-25 14:21:00 +0300

    Created whatifno_allocated_memory.c

--- Chapter13_Pointers/whatifno_allocated_memory.c
+++ Chapter13_Pointers/whatifno_allocated_memory.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    void* p1 = (void*)3;
-    int b = (int)&p1[5];
-    printf("b = %d ,(int)&a[5]=%p", b,((int)&p1[5]));
+    void* p1 = (void*)3;     //assigning address 0x3 to a pointer.
+    int b = (int)&p1[5];     //p1 + 5 so (0x3) + 5 ;
+    printf("b = %d ,(int)&a[5]=%p", b,((int)&p1[5]));   // This indicates an invalid memory address and invalid pointer arithmetic.
 }

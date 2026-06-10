bd96ce13cccb3174f683b54ba382487b69c71204
3xg3lin <krmutku76@gmail.com>
2026-06-10 16:43:52 +0300

    Updated carddeck_0.c

--- Chapter16_UsingMoreComplexStructures/carddeck_0.c
+++ Chapter16_UsingMoreComplexStructures/carddeck_0.c
@@ -33,6 +33,33 @@ typedef struct Card {
     bool isWild;
 } Card;
 
+int GetCardFaceValue(Card* pCard);
+int GetCardSuiteValue(Card* pCard);
+void PrintCard( Card* pCard );
+
 int main(void)  {
+    Card aCard;
+    aCard.suit = eDiamond;
+    aCard.suitValue = GetCardSuiteValue(pCard);
+    aCard.face = eSeven;
+    aCard.faceValue = GetCardFaceValue(pCard);
+    aCard.isWild = true;
+
+    PrintCard(&aCard);
+    printf("\n");
     return 0;
+}
+
+inline int GetCardFaceValue(Card* pCard) {
+    return (int)pCard->face;
+}
+
+inline int GetCardSuiteValue(Card* pCard) {
+    return (int)pCard->suitValue;
+}
+
+void PrintCard(Card* pCard){
+    char cardStr[20] = {0};
+    CardToString(pCard, cardStr);
+    printf("%18s", cardStr);
 }

e09b2033c9cb353515f3e271999e3ebd40c60649
kerem <krmutku76@gmail.com>
2026-07-16 00:28:25 +0300

    Created signedInt.c

---
+++ Chapter19_FormattedOutput/signedInt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+    int smallInt = 12;
+    int largeInt = 0x7fffffff;  // int32 max
+    int negativeInt = -smallInt;
+    unsigned anUnsigned = 130;
+    long long int reallyLargeInt = 0x7fffffffffffffff;
+
+    printf( " Signed Printf \n"
+              "  Name    right     left         zero      right     left\n"
+              "        aligned     aligned    filled    minimum     minimum    whatever\n"
+              "  Specifier    %%10d           %%-10d        %%-.10d          %%10.3d        %%-10.3d     %%d\n" );
+    printf(" -------------------- " "--------------- "
+        "--------------- " "---------------\n");
+
+}

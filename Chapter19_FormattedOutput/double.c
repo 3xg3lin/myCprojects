bbb22a85593b5687b08542b928d0ae23c4f421a9
kerem <krmutku76@gmail.com>
2026-07-16 19:51:14 +0300

    Updated double.c

--- Chapter19_FormattedOutput/double.c
+++ Chapter19_FormattedOutput/double.c
@@ -3,4 +3,26 @@
 int main(){
     double aDouble = 987654321.987654321;
 
+    printf("Use of the %%f, %%e, and %%E format specifiers:\n"
+              " Specifier Formatted Value\n");
+    printf(" %%f        [%f]    whatever\n", aDouble);
+    printf(" %%.3f      [%.3f]       3 decimal places\n", aDouble);
+    printf(" %%.9f      [%.8f]  8 decimal places\n", aDouble);
+    printf(" %%.0f      [%.0f]           no decimal places\n", aDouble);
+    printf(" %%#.0f     [%#.0f]          no decimal places, but decimal point\n", aDouble);
+    printf(" %%15.3f    [%15.3f]     3 decimals, 15 wide, right aligned\n",  aDouble);
+    printf(" %%-15.3f   [%-15.3f]     3 decimals, 15 wide, left aligned\n", aDouble);
+    printf(" %%e        [%e]        using exponential notation\n", aDouble);
+    printf(" %%E        [%E]        using EXPONENTIAL notation\n", aDouble);
+    printf(" %%.3e      [%.3e]           exponent with 3 decimal places\n", aDouble);
+    printf(" %%15.3e    [%15.3e]     exponent with 3 decimals, 15 wide\n", aDouble);
+    printf(" %%015.3e   [%015.3e]     exponent with 3 decimals, 15 wide, 0-fill\n", aDouble);
+    printf(" %% 15.3e   [% 15.3e]     exponent with 3 decimals, 15 wide, leave space for sign\n", aDouble);
+    printf(" %%+15.3e   [%+15.3e]     exponent with 3 decimals, 15 wide, show sign\n", aDouble);
+    printf(" %%+015.3e  [%+015.3e]     exponent with 3 decimals, 15 wide, show sign, 0-fill\n", aDouble);
+    printf(" %%.0e      [%.0e]               exponent with no decimals\n",  aDouble);
+    printf(" %%15.0e    [%15.0e]     exponent 15 wide, no decimals\n\n", aDouble);
+
+    printf(" %%a        [%a] hexidecimal version of double, exponent=2^p\n", aDouble);
+    printf(" %%A        [%A] HEXIDECIMAL version of double, exponent=2^P\n\n", aDouble);
 }

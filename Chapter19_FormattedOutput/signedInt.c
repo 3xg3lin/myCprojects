e11dbdf3d660b4d9a27cb00e759cf05294da65be
kerem <krmutku76@gmail.com>
2026-07-16 15:51:50 +0300

    Updated signedInt.c

--- Chapter19_FormattedOutput/signedInt.c
+++ Chapter19_FormattedOutput/signedInt.c
@@ -7,11 +7,36 @@ int main(void){
     unsigned anUnsigned = 130;
     long long int reallyLargeInt = 0x7fffffffffffffff;
 
-    printf( " Signed Printf \n"
-              "  Name         right        left        zero         right       left\n"
-              "              aligned      aligned     filled       minimum     minimum    whatever\n"
-              "  Specifier    %%10d        %%-10d       %%-.10d       %%10.3d     %%-10.3d     %%d\n" );
+    printf(" Signed Printf \n"
+              "  Name         right        left          zero           right          left\n"
+              "              aligned      aligned       filled         minimum        minimum    whatever\n"
+              "  Specifier    %%10d        %%-10d         %%-.10d         %%10.3d        %%-10.3d     %%d\n" );
     printf(" ---------  ---------- " " ----------- "
-        " ----------- " " ---------- " " ---------- " " ---------\n");
+        " ----------- " "    ---------- " "   ---------- " "   ---------\n");
+    printf("        [%10d]    [%-10d]  [%-.10d]   [%10.3d]   [%-10.3d]    [%d]\n", smallInt, smallInt, smallInt, smallInt, smallInt, smallInt);
+    printf("        [%10d]    [%-10d]  [%-.10d]   [%10.3d]   [%-10.3d]    [%d]\n", largeInt, largeInt, largeInt, largeInt, largeInt, largeInt);
+    printf("        [%10d]    [%-10d]  [%-.10d]   [%10.3d]   [%-10.3d]    [%d]\n", anUnsigned, anUnsigned, anUnsigned, anUnsigned, anUnsigned,anUnsigned);
+    printf("        [%10d]    [%-10d]  [%-.10d]  [%10.3d]   [%-10.3d]    [%d]\n", negativeInt, negativeInt, negativeInt, negativeInt, negativeInt, negativeInt);
 
+
+    printf("\n  Specifier               %%20lld                      %%-20lld                   %%-.20lld\n");
+    printf(" -----------           " " ---------"
+        "                   " "---------- " "                 " "---------\n");
+    printf("                  [%20lld]       [%-20lld]       [%-.20lld]\n", reallyLargeInt, reallyLargeInt, reallyLargeInt);
+    printf("\n  Specifier               %%20.3lld                      %%-20.3lld                   %%lld\n");
+    printf(" -----------           " " ---------"
+        "                   " "---------- " "                 " "---------\n");
+    printf("                  [%20.3lld]       [%-20.3lld]       [%lld]\n", reallyLargeInt, reallyLargeInt, reallyLargeInt);
+
+    printf( "Powers of 2: 2^0,       2^2,      2^4,        2^6,       2^8,     2^10\n" );
+    int k = 1;
+    for (int i = 0; i < 6; i++, k = k << 2) {
+        printf("          [%6d]    [%-6d]  [%-.6d]   [%6.3d]   [%-6.3d]    [%d]\n", k, k, k, k, k, k);
+    }
+
+    printf( "Powers of 9: 9^1,       9^2,      9^3,        9^4,       9^5\n" );
+    k = 9;
+    for (int i = 0; i < 5; i++, k = k * 9) {
+        printf("          [%6d]    [%-6d]  [%-.6d]   [%6.3d]   [%-6.3d]    [%d]\n", k, k, k, k, k, k);
+    }
 }

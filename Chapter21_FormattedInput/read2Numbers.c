e2a6f4467181ff356daf8d053ecaf713cf0faaeb
3xg3lin <krmutku76@gmail.com>
2026-08-01 17:42:09 +0300

    Created read2Numbers.c

---
+++ Chapter21_FormattedInput/read2Numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    int anInteger = -1;
+    double aDouble = -1.0;
+
+    printf("Enter an integer and a decimal number: ");
+    scanf("%d%lf", &anInteger, &aDouble);
+    printf("1. integer:  %d\n", anInteger);
+    printf("2.  double:  %lf\n\n", aDouble);
+
+    return 0;
+}

c21761f44e014f67be0d0ff3ceee7a551b102d78
kerem <krmutku76@gmail.com>
2026-08-03 01:48:09 +0300

    Updated readChar.c

--- Chapter21_FormattedInput/readChar.c
+++ Chapter21_FormattedInput/readChar.c
@@ -6,11 +6,11 @@ int main(void){
     int numScanned;
 
     printf("1st: Enter <integer><char><integer>: ");
-    numScanned = scanf("%d%c%d", &anInt1, &aChar, &anInt2);
+    numScanned = scanf("%d%c%d", &anInt1, &aChar, &anInt2);    // No space before %c -> %c does NOT skip whitespace, reads next char as-is
     printf("Values scanned = %d. Character selected: [%c]\n", numScanned, aChar);
 
     printf("2nd: Enter <integer> <char> <integer>: ");
-    numScanned = scanf("%d %c%d", &anInt1, &aChar, &anInt2);
+    numScanned = scanf("%d %c%d", &anInt1, &aChar, &anInt2);   // Space before %c -> tells scanf to skip any whitespace before reading the char
     printf("Values scanned = %d. Character selected: [%c]\n\n", numScanned, aChar);
 
     return 0;

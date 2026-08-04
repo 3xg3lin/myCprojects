cd92eda65e9e903e2ae1979f79fbb0dd68443b23
kerem <krmutku76@gmail.com>
2026-08-04 15:56:09 +0300

    Created readWidth.c

---
+++ Chapter21_FormattedInput/readWidth.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int year, month, day;
+    int numScanned;
+    while (printf("Enter mmddyyyy (and other character to quit): "), numScanned = scanf("%2d%2d%4d", &month, &day, &year), numScanned > 0) {
+        printf("%d/%d/%d\n", month, day, year);
+    }
+    printf("\nDone\n");
+    return 0;
+}

284e38a4da3cb4ed36a128e149d45386914a4824
kerem <krmutku76@gmail.com>
2026-08-04 17:24:53 +0300

    Created readDate.c

---
+++ Chapter21_FormattedInput/readDate.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int year, month, day;
+    int numScanned;
+    while (printf("Enter mm*dd*yyyy (and other character to quit): "), numScanned = scanf("%2d%*c%2d%*c%4d", &month, &day, &year), numScanned > 0) {
+        printf("%d/%d/%d\n", month, day, year);
+    }
+    printf("\nDone\n");
+    return 0;
+}

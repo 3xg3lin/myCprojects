3240b5ae830117f738bb3608ac33894e8d6d9aee
3xg3lin <krmutku76@gmail.com>
2026-08-04 17:32:48 +0300

    Updated readScanSet.c

--- Chapter21_FormattedInput/readScanSet.c
+++ Chapter21_FormattedInput/readScanSet.c
@@ -5,6 +5,9 @@ enum {kBufferSize = 80};
 int main(){
     char stringBuffer[kBufferSize] = {0};
     printf("Enter only vowels: ");
+    // THE SCANSET: "%[aeiouy]"
+    // This tells scanf to read characters ONLY as long as they match
+    // the characters inside the brackets.
     int numScanned = scanf("%[aeiouy]", stringBuffer);
     printf("Processed string: [%s]\n\n", stringBuffer);
 

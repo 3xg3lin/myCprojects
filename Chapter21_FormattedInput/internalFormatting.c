129350c801226028a063f3c18b9a681250704972
kerem <krmutku76@gmail.com>
2026-08-05 17:32:59 +0300

    Created internalFormatting.c

---
+++ Chapter21_FormattedInput/internalFormatting.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+enum {kBufferSize = 80};
+
+int main(){
+    int anInteger = -1;
+    double aDouble = -1.0;
+    int numScanned = 0, numPrinted = 0;
+
+    char sIn[] = "1234 5678.9012";
+    char sOut[kBufferSize];
+
+    memset(sOut, 0, kBufferSize);
+
+    printf("Using sscanf() on [%s]\n", sIn);
+    numScanned = sscanf(sIn, "%d%lf", &anInteger, &aDouble);
+    printf("sscanf() was able to assign %d values.\n", numScanned);
+    printf("1. integer: %d\n", anInteger);
+    printf("2. double: %lf\n\n", aDouble);
+
+    puts("Using sprintf() to format values to string buffer:");
+    numPrinted = sprintf(sOut, "integer=[%d] double=[%9.4lf]", anInteger, aDouble);
+    printf("%d characters in output string \"%s\"\n", numPrinted, sOut);
+    return 0;
+}

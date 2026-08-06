d9d465be4ac8d86abf9975073d947946397e433b
kerem <krmutku76@gmail.com>
2026-08-07 02:33:49 +0300

    Updated internalFormatting.c

--- Chapter21_FormattedInput/internalFormatting.c
+++ Chapter21_FormattedInput/internalFormatting.c
@@ -14,12 +14,16 @@ int main(){
     memset(sOut, 0, kBufferSize);
 
     printf("Using sscanf() on [%s]\n", sIn);
+    // sscanf: Reads formatted data FROM a string (sIn) instead of the keyboard.
+    // Returns the number of variables successfully assigned (0, 1, or 2).
     numScanned = sscanf(sIn, "%d%lf", &anInteger, &aDouble);
     printf("sscanf() was able to assign %d values.\n", numScanned);
     printf("1. integer: %d\n", anInteger);
     printf("2. double: %lf\n\n", aDouble);
 
     puts("Using sprintf() to format values to string buffer:");
+    // sprintf: Writes formatted data TO a string (sOut) instead of the screen.
+    // Returns the total number of characters written to the buffer.
     numPrinted = sprintf(sOut, "integer=[%d] double=[%9.4lf]", anInteger, aDouble);
     printf("%d characters in output string \"%s\"\n", numPrinted, sOut);
     return 0;

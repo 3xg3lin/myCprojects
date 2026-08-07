4b20aeff17a9357c2f7cef9ba1f94cb07c62c93a
kerem <krmutku76@gmail.com>
2026-08-07 18:07:45 +0300

    Created usingAtoi.c

---
+++ Chapter21_FormattedInput/usingAtoi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int anInteger = -1;
+    double aDouble = -1.0;
+
+    char sInteger[] = "1234";
+    char sDouble[] = "5678.9012";
+
+    printf("As strings: integer = \"%s\" double = \"%s\"\n", sInteger, sDouble);
+    anInteger = atoi(sInteger);
+    aDouble = atof(sDouble);
+    printf("As values: integer = [%d] double = [%lf]\n\n", anInteger, aDouble);
+    return 0;
+}

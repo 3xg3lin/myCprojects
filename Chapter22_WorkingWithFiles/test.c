cbac65aec550abe471d7438046cbbe1ae76b27f0
kerem <krmutku76@gmail.com>
2026-08-15 17:27:16 +0300

    Updated test.c

--- Chapter22_WorkingWithFiles/test.c
+++ Chapter22_WorkingWithFiles/test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    unsigned char a = 255;
-    unsigned char b = 254;
+    unsigned char a = 255;  // converts to -1
+    unsigned char b = 254; // converts to -2
     printf("%d \n%d \n",(signed char)a, (signed char)b);
 }

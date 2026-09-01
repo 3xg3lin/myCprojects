d493d28fccb1e7815b9fb19942216e53210528db
kerem <krmutku76@gmail.com>
2026-09-01 12:23:47 +0300

    Updated getoptFiles.c

--- Chapter23_UsingFileInputandOutput/getoptFiles.c
+++ Chapter23_UsingFileInputandOutput/getoptFiles.c
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,4 +10,34 @@ void usage(char* cmd){
     fprintf(stderr, "If -i inputFileName is not given," " stdin is used.\n");
     fprintf(stderr, "If -o outputFName is not given," " stdout is used.\n\n");
     exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char* argv[]){
+    int ch;
+    FILE* inputFile = NULL;
+    FILE* outputFile = NULL;
+
+    while ((ch = getopt(argc, argv, "i:o:h?")) != -1) {
+        switch (ch) {
+            case 'i':
+                if (NULL == (inputFile = fopen(optarg, "r"))) {
+                    fprintf(stderr, "inputFile \"%s\": %s\n", optarg, strerror(errno));
+                    exit(EXIT_FAILURE);
+                }
+                fprintf(stderr, "Using\"%s\" for input.\n", optarg);
+                break;
+            case 'o':
+                if (NULL == (outputFile = fopen(optarg, "a"))) {
+                    fprintf(stderr, "output file \"%s\": %s \n", optarg, strerror(errno));
+                    exit(EXIT_FAILURE);
+                }
+                fprintf(stderr, "Using\"%s\" for output.\n", optarg);
+                break;
+            case '?':
+            case 'h':
+            default:
+                usage(argv[0]);
+                break;
+        }
+    }
 }

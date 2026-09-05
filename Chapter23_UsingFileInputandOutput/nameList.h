2922599b1f8d9f17adccbdb999a09752632e3ccf
kerem <krmutku76@gmail.com>
2026-09-05 13:18:21 +0300

    Created nameList.h

--- Chapter23_UsingFileInputandOutput/nameList.h
+++ Chapter23_UsingFileInputandOutput/nameList.h
@@ -1,11 +1,29 @@
 #ifndef _NAME_LIST_H
 #define _NAME_LIST_H
 
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <type_traits>
 
 
+typedef char ListData ;
+typedef struct _Node {
+    struct _Node* pNext;
+    ListData* pData;
+}ListNode;
 
+typedef struct {
+    ListNode* pFirstNode;
+    int nodeCount;
+} NameList;
 
+ListNode* CreateListNode(char* pNameToAdd);
 
-
-
+void AddName(NameList* pNames, char* pNameToAdd);
+void DeleteNames(NameList* pNames);
+bool IsEmpty(NameList* pNames);
+void PrintNames(FILE* outputDesc, NameList* pNames);
+void OutOfStorage(void);
 #endif
